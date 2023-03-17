dfe972b4063a6a0ef5e878a2dcbdb12081f362cb
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-18 01:54:44 +0300

    moved Printer interface & CoonsolePrinter into common module

---
+++ common/src/main/java/com/megateam/lab/common/util/ConsolePrinter.java
@@ -0,0 +1,22 @@
+package com.megateam.lab.common.util;
+
+public class ConsolePrinter implements Printer
+{
+	@Override
+	public void print(String printable)
+	{
+		System.out.print(printable);
+	}
+
+	@Override
+	public void println(String printable)
+	{
+		System.out.println(printable);
+	}
+
+	@Override
+	public void printf(String printable, Object... args)
+	{
+		System.out.printf(printable, args);
+	}
+}
---
+++ common/src/main/java/com/megateam/lab/common/util/Printer.java
@@ -0,0 +1,8 @@
+package com.megateam.lab.common.util;
+
+public interface Printer
+{
+	void print(String printable);
+	void println(String printable);
+	void printf(String printable, Object... args);
+}
