a3b5d57320257b5ec5231cb99d27b43a4a3ad3bc
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:02:52 +0300

    single command executor implemented

---
+++ server/src/main/java/com/megateam/lab/server/executors/SingleCommandExecutor.java
@@ -0,0 +1,16 @@
+package com.megateam.lab.server.executors;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.exceptions.DatabaseException;
+import com.megateam.lab.common.exceptions.EnvException;
+import com.megateam.lab.common.util.Exchange;
+
+public class SingleCommandExecutor implements Executor
+{
+	@Override
+	public boolean execute(Exchange exchange) throws EnvException, DatabaseException
+	{
+		Command command = exchange.getCommand();
+		return command.execute();
+	}
+}
