8e738d1217cd7a6e9812bdbb94ac15ea444f2af2
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:05:43 +0300

    implemented dataclasses parsers

---
+++ client/src/main/java/com/megateam/lab/client/cli/TicketCLIParser.java
@@ -0,0 +1,223 @@
+package com.megateam.lab.client.cli;
+
+import com.megateam.lab.common.data.*;
+import com.megateam.lab.common.exceptions.UIException;
+import com.megateam.lab.common.exceptions.impl.UserDataInputInterruptedException;
+import com.megateam.lab.common.util.Printer;
+import com.megateam.lab.common.validation.DataClassesValidator;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TicketCLIParser
+{
+	@NonNull
+	private Scanner scanner;
+	@NonNull
+	private DataClassesValidator validator;
+	@NonNull
+	private Printer printer;
+
+	private CoordinatesCLIParser coordinatesCLIParser;
+	private VenueCLIParser venueCLIParser;
+
+	public TicketCLIParser(Scanner scanner, DataClassesValidator validator, Printer printer)
+	{
+		this.scanner = scanner;
+		this.validator = validator;
+		this.printer = printer;
+
+		this.coordinatesCLIParser = new CoordinatesCLIParser(scanner, validator, printer);
+		this.venueCLIParser = new VenueCLIParser(scanner, validator, printer);
+	}
+
+	private void proposeContinue() throws UIException
+	{
+		printer.print("Do you want to continue? [y/Y - for yes, other - for no]: ");
+		String userInput = scanner.nextLine().trim();
+		if (!"Y".equalsIgnoreCase(userInput))
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+	}
+
+	private String parseTicketName() throws UIException
+	{
+		printer.print("Enter ticket name (non-empty): ");
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			printer.println("You're not able to insert a null value for this variable. Try another value.");
+			proposeContinue();
+			return parseTicketName();
+		}
+
+		try
+		{
+			validator.validateTicketName(userInput);
+			return userInput;
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseTicketName();
+		}
+	}
+
+	private Coordinates parseTicketCoordinates() throws UIException
+	{
+		return this.coordinatesCLIParser.parseCoordinates();
+	}
+
+	private float parseTicketPrice() throws UIException
+	{
+		printer.print("Enter ticket price (float & greater than 0): ");
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			printer.println("You're not able to insert a null value for float variable. Try another value.");
+			proposeContinue();
+			return parseTicketPrice();
+		}
+
+		try
+		{
+			float x = Float.parseFloat(userInput);
+			validator.validateTicketPrice(x);
+			return x;
+		}
+		catch (NumberFormatException e)
+		{
+			printer.println("Ticket coordinate should be a float.");
+			proposeContinue();
+			return parseTicketPrice();
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseTicketPrice();
+		}
+	}
+
+	private String parseTicketComment() throws  UIException
+	{
+		printer.print("Enter ticket comment (non-empty): ");
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			printer.println("You're not able to insert a null value for this variable. Try another value.");
+			proposeContinue();
+			return parseTicketComment();
+		}
+
+		try
+		{
+			validator.validateTicketComment(userInput);
+			return userInput;
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseTicketComment();
+		}
+	}
+
+	private Boolean parseTicketRefundable() throws UIException
+	{
+		printer.print("Enter ticket refundable (non-empty): ");
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		try
+		{
+			Boolean refundable = Boolean.parseBoolean(userInput);
+			validator.validateTicketRefundable(refundable);
+			return refundable;
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseTicketRefundable();
+		}
+	}
+
+	private TicketType parseTicketType() throws UIException
+	{
+		printer.print(String.format("Enter ticket type (can be null) %s: ", Arrays.toString(TicketType.values())));
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			return null;
+		}
+
+		try
+		{
+			return TicketType.valueOf(userInput);
+		}
+		catch (IllegalArgumentException e)
+		{
+			printer.println("You should select ticket type from the listed ones.");
+			proposeContinue();
+			return parseTicketType();
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseTicketType();
+		}
+	}
+
+	private Venue parseTicketVenue() throws UIException
+	{
+		return this.venueCLIParser.parseVenue();
+	}
+
+	public Ticket parseTicket() throws UIException
+	{
+		printer.println("#### ENTERING TICKET ####");
+		String name = parseTicketName();
+		Coordinates coordinates = parseTicketCoordinates();
+		LocalDateTime creationDate = LocalDateTime.now(ZoneId.systemDefault());
+		float price = parseTicketPrice();
+		String comment = parseTicketComment();
+		Boolean ref = parseTicketRefundable();
+		TicketType type = parseTicketType();
+		Venue venue = parseTicketVenue();
+		printer.println("#### ENTERING TICKET ENDED ####");
+
+//		TODO: implement id autogen
+		return new Ticket(10, name, coordinates, creationDate, price, comment, ref, type, venue);
+	}
+
+
+
+}
---
+++ client/src/main/java/com/megateam/lab/client/cli/VenueCLIParser.java
@@ -0,0 +1,143 @@
+package com.megateam.lab.client.cli;
+
+import com.megateam.lab.common.data.Venue;
+import com.megateam.lab.common.data.VenueType;
+import com.megateam.lab.common.exceptions.UIException;
+import com.megateam.lab.common.exceptions.impl.UserDataInputInterruptedException;
+import com.megateam.lab.common.util.Printer;
+import com.megateam.lab.common.validation.DataClassesValidator;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+@RequiredArgsConstructor
+public class VenueCLIParser
+{
+	@NonNull
+	private Scanner scanner;
+	@NonNull
+	private DataClassesValidator validator;
+	@NonNull
+	private Printer printer;
+
+	private void proposeContinue() throws UIException
+	{
+		printer.print("Do you want to continue? [y/Y - for yes, other - for no]: ");
+		String userInput = scanner.nextLine().trim();
+		if (!"Y".equalsIgnoreCase(userInput))
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+	}
+
+	private String parseVenueName() throws UIException
+	{
+		printer.print("Enter venue name (non-empty): ");
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			printer.println("You're not able to insert a null value for this variable. Try another value.");
+			proposeContinue();
+			return parseVenueName();
+		}
+
+		try
+		{
+			validator.validateVenueName(userInput);
+			return userInput;
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseVenueName();
+		}
+	}
+
+	private Integer parseVenueCapacity() throws UIException
+	{
+		printer.print("Enter venue capacity (Integer & greater than 0): ");
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			printer.println("This variable cannot be null. Try another value.");
+			proposeContinue();
+			return parseVenueCapacity();
+		}
+
+		try
+		{
+			Integer capacity = Integer.parseInt(userInput);
+			validator.validateVenueCapacity(capacity);
+			return capacity;
+		}
+		catch (NumberFormatException e)
+		{
+			printer.println("Venue capacity should be an Integer.");
+			proposeContinue();
+			return parseVenueCapacity();
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseVenueCapacity();
+		}
+	}
+
+	private VenueType parseVenueType() throws UIException
+	{
+		printer.print(String.format("Enter venue type (can be null) %s: ", Arrays.toString(VenueType.values())));
+
+		if (!scanner.hasNextLine())
+			throw new UserDataInputInterruptedException("Data input successfully interrupted");
+
+		String userInput = scanner.nextLine().trim();
+
+		if ("".equals(userInput))
+		{
+			return null;
+		}
+
+		try
+		{
+			return VenueType.valueOf(userInput);
+		}
+		catch (IllegalArgumentException e)
+		{
+			printer.println("You should select type from the listed ones.");
+			proposeContinue();
+			return parseVenueType();
+		}
+		catch (Exception e)
+		{
+			printer.println(e.getMessage());
+			proposeContinue();
+			return parseVenueType();
+		}
+	}
+
+	public Venue parseVenue() throws UIException
+	{
+		printer.println("#### ENTERING VENUE ####");
+		String name = parseVenueName();
+		Integer capacity = parseVenueCapacity();
+		VenueType type = parseVenueType();
+		printer.println("#### ENTERING VENUE ENDED ####");
+
+//		TODO: implement id autogen
+		return new Venue(10L, name, capacity, type);
+	}
+
+
+}
