aa9f762d9faaecac30e97de0baa21d8d914a1d0d
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:07:06 +0300

    implemented info command

---
+++ common/src/main/java/com/megateam/lab/common/command/impl/InfoCommand.java
@@ -0,0 +1,38 @@
+package com.megateam.lab.common.command.impl;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.command.CommandSource;
+import com.megateam.lab.common.command.CommandUsesElements;
+import com.megateam.lab.common.dao.Dao;
+import com.megateam.lab.common.data.Ticket;
+import com.megateam.lab.common.exceptions.DatabaseException;
+import com.megateam.lab.common.exceptions.EnvException;
+import com.megateam.lab.common.util.Printer;
+import lombok.NonNull;
+
+import java.util.Collections;
+
+public class InfoCommand extends Command
+{
+	@NonNull
+	private Dao<Ticket> dao;
+
+	public InfoCommand(
+			@NonNull Printer printer,
+			@NonNull CommandSource source,
+			@NonNull Dao<Ticket> dao
+			)
+	{
+		super(printer, source, CommandUsesElements.NOT_USES, Collections.emptyList());
+		this.dao = dao;
+	}
+
+	@Override
+	public boolean execute() throws EnvException, DatabaseException
+	{
+		printer.println("Collection type: ArrayList");
+		printer.println("Creation date: " + dao.getCreationDate());
+		printer.println("Elements count: " + dao.size());
+		return true;
+	}
+}
