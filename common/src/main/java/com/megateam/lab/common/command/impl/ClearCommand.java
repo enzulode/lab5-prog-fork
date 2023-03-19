4d24e5307375f4b998251e20e7ffe5b31236e66f
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:06:36 +0300

    implemented clear command

---
+++ common/src/main/java/com/megateam/lab/common/command/impl/ClearCommand.java
@@ -0,0 +1,42 @@
+package com.megateam.lab.common.command.impl;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.command.CommandSource;
+import com.megateam.lab.common.command.CommandUsesElements;
+import com.megateam.lab.common.dao.Dao;
+import com.megateam.lab.common.data.Ticket;
+import com.megateam.lab.common.exceptions.DatabaseException;
+import com.megateam.lab.common.exceptions.EnvException;
+import com.megateam.lab.common.util.Printer;
+import lombok.NonNull;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ClearCommand extends Command
+{
+	@NonNull
+	private Dao<Ticket> dao;
+	public ClearCommand(
+			@NonNull Printer printer,
+			@NonNull CommandSource source,
+			Dao<Ticket> dao
+	)
+	{
+		super(
+				printer,
+				source,
+				CommandUsesElements.NOT_USES,
+				Collections.emptyList()
+		);
+		this.dao = dao;
+	}
+
+	@Override
+	public boolean execute() throws EnvException, DatabaseException
+	{
+		dao.clear();
+		printer.println("Collection successfully cleared.");
+		return true;
+	}
+}
