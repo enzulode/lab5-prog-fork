f07bb85d8440133e332445644051fb94cae2dc6c
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:07:42 +0300

    implemented remove_first & remove_last commands

---
+++ common/src/main/java/com/megateam/lab/common/command/impl/RemoveLastCommand.java
@@ -0,0 +1,36 @@
+package com.megateam.lab.common.command.impl;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.command.CommandSource;
+import com.megateam.lab.common.command.CommandUsesElements;
+import com.megateam.lab.common.dao.Dao;
+import com.megateam.lab.common.data.Ticket;
+import com.megateam.lab.common.exceptions.DatabaseException;
+import com.megateam.lab.common.exceptions.EnvException;
+import com.megateam.lab.common.util.Printer;
+import lombok.NonNull;
+
+import java.util.Collections;
+
+public class RemoveLastCommand extends Command
+{
+	@NonNull
+	private Dao<Ticket> dao;
+
+	public RemoveLastCommand(
+			@NonNull Printer printer,
+			@NonNull CommandSource source,
+			@NonNull Dao<Ticket> dao
+	)
+	{
+		super(printer, source, CommandUsesElements.NOT_USES, Collections.emptyList());
+		this.dao = dao;
+	}
+
+	@Override
+	public boolean execute() throws EnvException, DatabaseException
+	{
+		dao.removeLast();
+		return true;
+	}
+}
