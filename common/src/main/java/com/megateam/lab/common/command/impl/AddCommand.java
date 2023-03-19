1317330c63ecb118cda24701a5fee0cc8d57f9b0
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:06:19 +0300

    implemented add command

---
+++ common/src/main/java/com/megateam/lab/common/command/impl/AddCommand.java
@@ -0,0 +1,37 @@
+package com.megateam.lab.common.command.impl;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.command.CommandSource;
+import com.megateam.lab.common.command.CommandUsesElements;
+import com.megateam.lab.common.dao.Dao;
+import com.megateam.lab.common.data.Ticket;
+import com.megateam.lab.common.util.Printer;
+import lombok.NonNull;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AddCommand extends Command
+{
+	private CommandSource source;
+	private List<String> args;
+	private Dao<Ticket> dao;
+
+	public AddCommand(
+			@NonNull Printer printer,
+			@NonNull CommandSource source,
+			@NonNull Dao<Ticket> dao
+	)
+	{
+		super(printer, source, CommandUsesElements.USES, Collections.emptyList());
+		this.dao = dao;
+	}
+
+	@Override
+	public boolean execute()
+	{
+		Ticket ticket = (Ticket) additionalArgs.get(0);
+		dao.add(ticket);
+		return true;
+	}
+}
