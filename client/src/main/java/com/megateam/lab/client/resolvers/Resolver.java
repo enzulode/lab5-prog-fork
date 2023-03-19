1210f3d22b603ca5a8114e496da9bcac1a82f1e2
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-19 16:04:43 +0300

    Resolver moved to a client side & SingleCommandResolver implemented

---
+++ client/src/main/java/com/megateam/lab/client/resolvers/Resolver.java
@@ -0,0 +1,24 @@
+package com.megateam.lab.client.resolvers;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.command.impl.AddCommand;
+import com.megateam.lab.common.exceptions.ResolverException;
+import com.megateam.lab.common.exceptions.impl.DefaultResolverUsedException;
+import com.megateam.lab.common.util.Exchange;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+public interface Resolver
+{
+	default Exchange resolve(String line) throws ResolverException
+	{
+		throw new DefaultResolverUsedException("Trying to invoke default resolve method. This is not possible.");
+	}
+
+	default Optional<List<Exchange>> resolve(File script) throws ResolverException
+	{
+		throw new DefaultResolverUsedException("Trying to invoke default resolve method. This is not possible.");
+	}
+}
---
+++ client/src/main/java/com/megateam/lab/client/resolvers/SingleCommandResolver.java
@@ -0,0 +1,40 @@
+package com.megateam.lab.client.resolvers;
+
+import com.megateam.lab.common.command.Command;
+import com.megateam.lab.common.command.CommandFactory;
+import com.megateam.lab.common.command.CommandSource;
+import com.megateam.lab.common.dao.Dao;
+import com.megateam.lab.common.data.Ticket;
+import com.megateam.lab.common.exceptions.ResolverException;
+import com.megateam.lab.common.util.Exchange;
+import com.megateam.lab.common.util.Printer;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SingleCommandResolver implements Resolver
+{
+	@NonNull
+	private Printer printer;
+	@NonNull
+	private Dao<Ticket> dao;
+
+	@Override
+	public Exchange resolve(String line) throws ResolverException
+	{
+		String[] sepLine = line.split(" ");
+		List<String> args = List.of(Arrays.copyOfRange(sepLine, 1, sepLine.length));
+		Command command = CommandFactory.newCommand(printer, CommandSource.CONSOLE, sepLine[0], args, dao);
+
+		Exchange exchange = Exchange
+				.builder()
+				.setCommand(command)
+				.setArgs(args)
+				.build();
+
+		return exchange;
+	}
+}
